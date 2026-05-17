7fff0e50ac258369feae3ee5c529ddc5a006535d
GitHub Action <action@github.com>
2026-05-17 04:01:20 +0000

    Update swift6 SDK [skip ci]

--- Sources/OpenAPIClient/Models/OrderRefundAdd.swift
+++ Sources/OpenAPIClient/Models/OrderRefundAdd.swift
@@ -27,12 +27,14 @@ public struct OrderRefundAdd: Sendable, Codable, JSONEncodable, Hashable {
     public var sendNotifications: Bool? = false
     /** Specifies an order creation date in format Y-m-d H:i:s */
     public var date: String?
+    /** Store Id */
+    public var storeId: String?
     /** Indicates whether refund type is online */
     public var isOnline: Bool? = false
     /** A unique identifier associated with a specific request. Repeated requests with the same <strong>idempotency_key</strong> return a cached response without re-executing the business logic. <strong>Please note that the cache lifetime is 15 minutes.</strong> */
     public var idempotencyKey: String?
 
-    public init(orderId: String? = nil, items: [OrderRefundAddItemsInner]? = nil, totalPrice: Double? = nil, shippingPrice: Double? = nil, feePrice: Double? = nil, message: String? = nil, itemRestock: Bool? = false, sendNotifications: Bool? = false, date: String? = nil, isOnline: Bool? = false, idempotencyKey: String? = nil) {
+    public init(orderId: String? = nil, items: [OrderRefundAddItemsInner]? = nil, totalPrice: Double? = nil, shippingPrice: Double? = nil, feePrice: Double? = nil, message: String? = nil, itemRestock: Bool? = false, sendNotifications: Bool? = false, date: String? = nil, storeId: String? = nil, isOnline: Bool? = false, idempotencyKey: String? = nil) {
         self.orderId = orderId
         self.items = items
         self.totalPrice = totalPrice
@@ -42,6 +44,7 @@ public struct OrderRefundAdd: Sendable, Codable, JSONEncodable, Hashable {
         self.itemRestock = itemRestock
         self.sendNotifications = sendNotifications
         self.date = date
+        self.storeId = storeId
         self.isOnline = isOnline
         self.idempotencyKey = idempotencyKey
     }
@@ -56,6 +59,7 @@ public struct OrderRefundAdd: Sendable, Codable, JSONEncodable, Hashable {
         case itemRestock = "item_restock"
         case sendNotifications = "send_notifications"
         case date
+        case storeId = "store_id"
         case isOnline = "is_online"
         case idempotencyKey = "idempotency_key"
     }
@@ -73,6 +77,7 @@ public struct OrderRefundAdd: Sendable, Codable, JSONEncodable, Hashable {
         try container.encodeIfPresent(itemRestock, forKey: .itemRestock)
         try container.encodeIfPresent(sendNotifications, forKey: .sendNotifications)
         try container.encodeIfPresent(date, forKey: .date)
+        try container.encodeIfPresent(storeId, forKey: .storeId)
         try container.encodeIfPresent(isOnline, forKey: .isOnline)
         try container.encodeIfPresent(idempotencyKey, forKey: .idempotencyKey)
     }
